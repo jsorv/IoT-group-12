a608c5ae56e78d30fe71fddf3b3e925b3f492c91
Jessica <jessicasuarezc@outlook.com>
2025-12-08 00:35:40 +0200

    Added HIVE config separate | Issues

--- HW_Jessica.py
+++ HW_Jessica.py
@@ -1,6 +1,62 @@
 from machine import Pin, PWM, ADC, SoftI2C
 from machine_i2c_lcd import I2cLcd
+from umqtt.simple import MQTTClient
 from time import sleep, time
+import config
+import network
+import socket
+import ssl
+import sys
+
+# WIFI SETUP - CONNECTION
+ssid = config.ssid
+pwd = config.pwd
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, pwd)
+
+max_retries = 10
+retries = 0
+#Waits for the connection to established
+#Waits for maximun 20 seconds before continuing
+while not wlan.isconnected() and retries < max_retries:
+    print(f"Attempt {retries + 1}/{max_retries}...")
+    sleep(2)
+    retries += 1
+    
+if wlan.isconnected():
+    print(f"Congratulations WiFi connected succesfully!")
+else:
+    print("Oh no!Failed to connect to the Wi-Fi!")
+    sys.exit()
+
+# set up socket and listen on port 80
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(addr)
+s.listen(1)  # Listen for incoming connections
+
+print('[INFO] Listening on', addr)
+
+
+#MQTT BROKER SETUP:
+broker = "421b822c7f5243ffba8964c47409268d.s1.eu.hivemq.cloud"
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+context.verify_mode = ssl.CERT_NONE
+
+client = MQTTClient(
+    client_id=b'hello',
+    server=config.MQTT_BROKER,
+    port=config.MQTT_PORT,               # TLS port for HiveMQ
+    user=config.MQTT_USER,
+    password=config.MQTT_PWD,
+    ssl=context              # <- keep context as TA requires
+)
+
+client.connect()
+
 
 # Define the LCD I2C address and dimensions
 I2C_ADDR = 0x27
@@ -29,6 +85,26 @@ lcd.putstr("Security System Active")
 sleep(2)
 lcd.clear()
 lcd.backlight_off()
+
+#HTML Code display
+status_char = "System Active"
+# generate html
+def generate_html(status):
+    html = f"""\
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+
+    <!DOCTYPE html>
+    <html>
+      <head><title>Security alarm system</title></head>
+      <body>
+          <h1 style='color:red, text-align:center'>SECURITY ALARM SYSTEM</h1>
+          <h2>Status: {status}</h2>
+          <p>This is static content for now.</p>
+      </body>
+    </html>
+    """
+    return str(html)
 
 try:
     while True:
@@ -60,6 +136,20 @@ try:
             sleep(1)
             lcd.clear()
             lcd.backlight_off()
+
+        try:
+            cl, addr = s.accept()
+            print('[INFO] Client connected from', addr)
+            request = cl.recv(1024)
+            print('[INFO] Request:', request)
+
+            # send static HTML
+            response = generate_html(status_char)
+            cl.send(response)
+            cl.close()
+
+        except OSError:
+            pass  # No client connected; continue loop
 
         sleep(0.1)
 
---
+++ config.py
@@ -0,0 +1,6 @@
+ssid = "JESSICA"
+pwd = "1234567890"
+MQTT_BROKER = '421b822c7f5243ffba8964c47409268d.s1.eu.hivemq.cloud' # broker/server URL
+MQTT_PORT= 8883
+MQTT_USER = 'jssuarezc'  # access username
+MQTT_PWD = 'dr34msc#m3trU3' # access pwd
